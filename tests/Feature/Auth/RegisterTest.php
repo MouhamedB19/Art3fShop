9812e78342a919f55486320de4ba422d75d07ced
Mouhamed <mouhamed-ababacar.ba@uha.fr>
2026-09-02 23:24:12 +0200

    Ajout de tests pour l'enregistrement des utilisateurs, y compris les clients et les artistes. Amélioration des assertions de statut de réponse. Amélioration de la méthode d'enregistrement des utilisateurs.

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -13,7 +13,7 @@ class RegisterTest extends TestCase
      */
     public function test_register_client(): void
     {
-        $response = $this->postJson('/api/register',[
+        $responseClient = $this->postJson('/api/register',[
             'nom' => 'Doe',
             'prenom' => 'John',
             'email' => 'john.doe@example.com',
@@ -22,6 +22,32 @@ class RegisterTest extends TestCase
             'role' => 'acheteur'
         ]);
 
-        $response->assertStatus(201);
+        
+        $responseClient->assertStatus(201);
+    }
+
+    public function test_register_artiste(): void
+    {
+        $responseArtiste = $this->postJson('/api/register',[
+            'nom' => 'Smith',
+            'prenom' => 'Jane',
+            'email' => 'jane.smith@example.test',
+            'password' => 'password123',
+            'password_confirmation' => 'password123',
+            'role' => 'artiste',
+            'nom_d_artiste' => 'JaneSmithArt',
+            'bio' => 'Nouvel artiste',
+            'photo' => 'photos/jane_smith.jpg',
+            'iban' => 'FR7630006000011234567890189',
+            'a_la_une' => false,
+            'Est_Artiste_Art3f' => false,
+            'CV' => 'CVs/jane_smith_cv.pdf',
+            'code_postal' => '75001',
+            'adresse' => '123 Rue de l\'Art',
+            'nom_ville' => 'Paris',
+            'nom_pays' => "France",
+        ]);
+
+        $responseArtiste->assertStatus(201);
     }
 }
